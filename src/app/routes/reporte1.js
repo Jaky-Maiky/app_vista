0449b9451b81c28cd472f2809c47b7392839dbca
Jhonny Guevara <jhonny.guevara@pucese.edu.ec>
2020-06-10 12:18:32 -0500

    actualizacion final

--- src/app/routes/reporte1.js
+++ src/app/routes/reporte1.js
@@ -5,7 +5,7 @@ module.exports = app => {
   const connection = dbConnection();
 
   app.get('/reporte1', (req, res) => {
-    connection.query('SELECT * FROM canciones_escuchadas', (err, result) => {
+    connection.query('SELECT * FROM reporte_de_canciones_mas_escuchados', (err, result) => {
       res.render('reportes/reporte1', {
         reporte1: result
       });
--- src/app/routes/reporte2.js
+++ src/app/routes/reporte2.js
@@ -5,7 +5,7 @@ module.exports = app => {
   const connection = dbConnection();
 
   app.get('/reporte2', (req, res) => {
-    connection.query('SELECT * FROM reporte_usuarios', (err, result) => {
+    connection.query('SELECT * FROM reporte_de_ingresos_de_usuarios', (err, result) => {
       res.render('reportes/reporte2', {
         reporte2: result
       });
